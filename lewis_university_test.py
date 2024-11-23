a0ee4b2e891210cbd77930b3ddfa10b81b4f4b45
Amruth291 <amruthvailala@gmail.com>
2024-11-23 00:07:51 -0700

    Update lewis_university_test.py

--- lewis_university_test.py
+++ lewis_university_test.py
@@ -10,7 +10,8 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 # Set up WebDriver
-driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=chrome_options)
+
 
 def test_homepage_title():
     print("Opening Lewis University website...")
